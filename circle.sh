07ce2ce5f6c759c789995bc0837f9d6211b40c38
Rohan Singh <rohan@spotify.com>
2014-10-15 16:48:35 -0400

    CircleCI: Collect Docker debug and traffic logs
    * Run Docker with debug logging and collect the docker.log artifact.
    
    * Use socat as a proxy for Helios <-> Docker communication, and collect
      all logged traffic in the socat.log artifact.

--- circle.sh
+++ circle.sh
@@ -2,26 +2,41 @@
 
 case "$1" in
   pre_machine)
-    # have docker bind to localhost
-    docker_opts='DOCKER_OPTS="$DOCKER_OPTS -H tcp://127.0.0.1:2375"'
+    sudo apt-get install socat
+
+    # have docker write debug logs
+    docker_opts='DOCKER_OPTS="$DOCKER_OPTS -D"'
     sudo sh -c "echo '$docker_opts' >> /etc/default/docker"
 
     cat /etc/default/docker
 
     ;;
 
-  dependencies)
-    mvn clean install -T 2 -Dmaven.javadoc.skip=true -DskipTests=true -B -V
+  post_machine)
+    sudo chown ubuntu:ubuntu /var/log/upstart/docker.log
 
     ;;
 
-  pre_test)
+  dependencies)
     # clean the artifacts dir from the previous build
     rm -rf artifacts && mkdir artifacts
+
+    mvn clean install -T 2 -Dmaven.javadoc.skip=true -DskipTests=true -B -V
+
+    # collect artifacts into the artifacts dir
+    find . -regex ".*/target/.*-[0-9]\.jar" | xargs -I {} mv {} artifacts
+    find . -regex ".*/target/.*-SNAPSHOT\.jar" | xargs -I {} mv {} artifacts
+    find . -regex ".*/target/.*\.deb" | xargs -I {} mv {} artifacts
 
     ;;
 
   test)
+    # use socat so we can log all traffic to/from docker daemon
+    socat -v TCP4-LISTEN:2375,fork,reuseaddr UNIX-CONNECT:/var/run/docker.sock &> artifacts/socat.log &
+    sleep 2
+
+    docker info
+
     # expected parallelism: 2x. needs to be set in the project settings via CircleCI's UI.
     case $CIRCLE_NODE_INDEX in
       0)
@@ -42,10 +57,7 @@ case "$1" in
     ;;
 
   post_test)
-    # collect artifacts into the artifacts dir
-    find . -regex ".*/target/.*-[0-9]\.jar" | xargs -I {} mv {} artifacts
-    find . -regex ".*/target/.*-SNAPSHOT\.jar" | xargs -I {} mv {} artifacts
-    find . -regex ".*/target/.*\.deb" | xargs -I {} mv {} artifacts
+    sudo cp /var/log/upstart/docker.log artifacts
 
     ;;
 
